578cb6a36d4f065387bc64e51cbf5c5bc76adae9
Morgan Touverey Quilling <morgan@mtq.io>
2024-06-07 16:14:40 +0200

    refactor(cs): add utility method I just added to the wiki

---
+++ HallOfFame/Utils/LocalizationExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Colossal.Localization;
+using Game.SceneFlow;
+
+namespace HallOfFame.Utils;
+
+internal static class LocalizationExtensions {
+    internal static LocalizationDictionary LocalizationDictionary =>
+        GameManager.instance.localizationManager.activeDictionary;
+
+    /// <summary>
+    /// One-off translation from a key.
+    /// If the key is not found in the dictionary, it is returned as is.
+    /// </summary>
+    internal static string Translate(this string key) {
+        return LocalizationExtensions.LocalizationDictionary
+            .TryGetValue(key, out var value)
+            ? value
+            : key;
+    }
+
+    /// <summary>
+    /// Same as <see cref="Translate(string)"/> but with variables
+    /// interpolation.
+    /// </summary>
+    /// <param name="key">
+    /// A key that points to a string that
+    /// <see cref="string.Format(string,object[])"/> can format.
+    /// </param>
+    /// <param name="args">Values to interpolate</param>
+    internal static string Translate(this string key, params object[] args) {
+        return string.Format(key.Translate(), args);
+    }
+}
