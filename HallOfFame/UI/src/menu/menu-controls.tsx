4883dd76c5be681ca6b5997901e4f9d0a15e3448
Morgan Touverey Quilling <morgan@mtq.io>
2024-10-13 11:46:32 +0200

    feat(ui, menu): shorter number formatting for city population

--- HallOfFame/UI/src/menu/menu-controls.tsx
+++ HallOfFame/UI/src/menu/menu-controls.tsx
@@ -244,15 +244,7 @@ function MenuControlsScreenshotLabels({
 
                     <span>
                         <img src='Media/Game/Icons/Population.svg' />
-                        <LocalizedNumber
-                            value={
-                                screenshot.cityPopulation < 1000
-                                    ? screenshot.cityPopulation
-                                    : Math.round(
-                                          screenshot.cityPopulation / 1000
-                                      ) * 1000
-                            }
-                        />
+                        {formatBigNumber(screenshot.cityPopulation, translate)}
                     </span>
                 </>
             )}
@@ -406,5 +398,39 @@ function locElementToReactNode(
         <LocalizedString id={element.id} fallback={element.value} />
     ) : (
         <>{fallback}</>
+    );
+}
+
+/**
+ * Formats a big number to a human-readable string, applying special formatting
+ * rules depending on the number's magnitude.
+ */
+function formatBigNumber(
+    num: number,
+    translate: Localization['translate']
+): string {
+    let numStr: string;
+
+    if (num < 1000) {
+        // No formatting on small numbers.
+        numStr = num.toString();
+    } else if (num < 10_000) {
+        // Formats as "9.9K", precision .1K.
+        numStr = `${(num / 1000).toFixed(1)} K`;
+    } else if (num < 1_000_000) {
+        // Formats as "99K", rounded, precision 1K.
+        numStr = `${Math.round(num / 1000)} K`;
+    } else {
+        // Formats as "9.9M", precision .1M.
+        numStr = `${(num / 1_000_000).toFixed(1)} M`;
+    }
+
+    return (
+        numStr
+            // Remove trailing zeros.
+            .replace('.0', '')
+            // Replace decimal separator.
+            // biome-ignore lint/style/noNonNullAssertion: fallback value
+            .replace('.', translate('Common.DECIMAL_SEPARATOR', '.')!)
     );
 }
