1e4740fd0823903856b6a707b3bf13e19b774295
Morgan Touverey Quilling <morgan@mtq.io>
2025-06-14 19:39:00 +0200

    feat(ui, area-game): add tooltip on maximize button

--- HallOfFame/UI/src/area-game/screenshot-upload-panel.tsx
+++ HallOfFame/UI/src/area-game/screenshot-upload-panel.tsx
@@ -1,6 +1,6 @@
 import { bindValue, trigger, useValue } from 'cs2/api';
 import { type Localization, LocalizedNumber, LocalizedString, useLocalization } from 'cs2/l10n';
-import { Button, Icon } from 'cs2/ui';
+import { Button, Icon, Tooltip } from 'cs2/ui';
 import { type CSSProperties, type ReactElement, useEffect, useMemo } from 'react';
 import cloudArrowUpSolidSrc from '../icons/fontawesome/cloud-arrow-up-solid.svg';
 import populationSrc from '../icons/paradox/population.svg';
@@ -214,18 +214,20 @@ function ScreenshotUploadPanelImage({
         </DescriptionTooltip>
       )}
 
-      <Button
-        variant='round'
-        selectSound='open-panel'
-        onSelect={() => showFullscreenImage(screenshotSnapshot.imageUri)}
-        className={`${styles.screenshotUploadPanelImageMagnifyButton} ${
-          uploadProgress ? styles.screenshotUploadPanelImageHidden : ''
-        }`}>
-        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
-          {/* Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. */}
-          <path d='M32 32C14.3 32 0 46.3 0 64l0 96c0 17.7 14.3 32 32 32s32-14.3 32-32l0-64 64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 96c0 17.7 14.3 32 32 32l96 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0 0-64zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32l64 0 0 64c0 17.7 14.3 32 32 32s32-14.3 32-32l0-96c0-17.7-14.3-32-32-32l-96 0zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l96 0c17.7 0 32-14.3 32-32l0-96z' />
-        </svg>
-      </Button>
+      <Tooltip tooltip={translate('HallOfFame.UI.Game.ScreenshotUploadPanel.MAXIMIZE_TOOLTIP')}>
+        <Button
+          variant='round'
+          selectSound='open-panel'
+          onSelect={() => showFullscreenImage(screenshotSnapshot.imageUri)}
+          className={`${styles.screenshotUploadPanelImageMagnifyButton} ${
+            uploadProgress ? styles.screenshotUploadPanelImageHidden : ''
+          }`}>
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
+            {/* Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. */}
+            <path d='M32 32C14.3 32 0 46.3 0 64l0 96c0 17.7 14.3 32 32 32s32-14.3 32-32l0-64 64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 96c0 17.7 14.3 32 32 32l96 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0 0-64zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32l64 0 0 64c0 17.7 14.3 32 32 32s32-14.3 32-32l0-96c0-17.7-14.3-32-32-32l-96 0zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l96 0c17.7 0 32-14.3 32-32l0-96z' />
+          </svg>
+        </Button>
+      </Tooltip>
 
       {uploadProgress && (
         <div className={styles.screenshotUploadPanelImageUploadProgress}>
